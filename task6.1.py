b5cbfdebe29e00910f2c42de2fd97f6143a8b998
Roaoch <103529423+Roaoch@users.noreply.github.com>
2022-11-25 17:55:49 +0500

    f

--- task6.1.py
+++ task6.1.py
@@ -162,19 +162,15 @@ class InputConnect:
                     vacancy.area_name,
                     1
                 )
-        self.all_salary_level = {key: int(value / self.all_vacancies_count[key]) for key, value in self.all_salary_level.items()}
+        self.all_salary_level = \
+            {key: int(value / self.all_vacancies_count[key]) for key, value in self.all_salary_level.items()}
         self.salary_level = {key: int(value / self.vacancies_count[key]) for key, value in self.salary_level.items()}
 
         if len(self.salary_level) == 0:
             self.salary_level = {key: 0 for key in self.all_vacancies_count.keys()}
             self.vacancies_count = {key: 0 for key in self.all_vacancies_count.keys()}
 
-        f = sum(temp_all_by_city_count.values())
-        self.vacancies_part = dict(sorted(
-            [(key, float("{:.4f}".format(value / f))) for key, value in temp_all_by_city_count.items() if value / f >= 0.01],
-            key=lambda e: e[1],
-            reverse=True
-        ))
+        self.vacancies_part = self.__get_vacancy_part(temp_all_by_city_count)
         self.by_city_level = dict(sorted(
             [(key, int(temp_by_city_level[key] / temp_all_by_city_count[key])) for key in self.vacancies_part.keys()],
             key=lambda e: (e[1], -len(e[0])),
@@ -188,6 +184,20 @@ class InputConnect:
         print(f"Динамика количества вакансий по годам для выбранной профессии: {self.vacancies_count}")
         print(f"Уровень зарплат по городам (в порядке убывания): {self.__slice_dict(self.by_city_level, 10)}")
         print(f"Доля вакансий по городам (в порядке убывания): {self.__slice_dict(self.vacancies_part, 10)}")
+
+    @staticmethod
+    def __get_vacancy_part(by_city_count: Dict[str, int]):
+        result = []
+        all_vacancy_count = sum(by_city_count.values())
+        for key, value in by_city_count.items():
+            part = value / all_vacancy_count
+            if part >= 0.01:
+                result.append((key, float("{:.4f}".format(part))))
+        return dict(sorted(
+            result,
+            key=lambda e: e[1],
+            reverse=True
+        ))
 
     @staticmethod
     def __add_to_or_update(dictionary: Dict[any, any], key: any, value: any) -> None:
